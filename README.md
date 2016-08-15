3a672846c715b00872ce05fe894a92f978bdc064
Robin van Kekem <Robin van Kekem>
2016-08-15 12:49:10 +0200

    Add more logging Update z-wave driver

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # BeNext
 
 This app adds support for BeNext devices in Homey.
+
+## Currently supported devices:
+
+* Alarm sound (recognizes the device but until a future firmware update of Homey unable to activate)
--- drivers/alarmsound/driver.js
+++ drivers/alarmsound/driver.js
@@ -3,7 +3,7 @@
 const path			= require('path');
 const ZwaveDriver	= require('homey-zwavedriver');
 
-var devices = {};
+var devices = [];
 
 // http://www.vesternet.com/downloads/dl/file/id/1024/product/2155/z_wave_benext_alarm_sound_manual.pdf
 
@@ -57,7 +57,8 @@ module.exports = new ZwaveDriver( path.basename(__dirname), {
 
 module.exports.init = function( devices_data, callback ) {
 	Homey.log('');
-	Homey.log("init method", devices_data);
+	Homey.log("init method");
+	Homey.log("devices_data", devices_data);
 	devices_data.forEach(initDevice);
 
     // let Homey know the driver is ready
@@ -75,7 +76,8 @@ module.exports.added = function( device_data, callback ) {
 // the `delete` method is called when a device has been deleted by a user
 module.exports.deleted = function( device_data, callback ) {
 	Homey.log('');
-	Homey.log("deleted method", device_data);
+	Homey.log("deleted method");
+	Homey.log('device_data.id:', device_data.id);
     delete devices[ device_data.id ];
     callback( null, true );
 }
@@ -85,7 +87,14 @@ function initDevice( device_data ) {
 	Homey.log('');
 	Homey.log('initDevice');
 	Homey.log('device_data', device_data);
-	//devices.push[device_data];
+
+	var token = device_data.token;
+	Homey.log('token:', token);
+
+	var node = module.exports.nodes;
+	Homey.log('node:', node);
+
+	devices.push(device_data);
 }
 
 // a helper method to get a device from the devices list by it's device_data object
@@ -103,11 +112,12 @@ Homey.manager('flow').on('action.sound_alarm', function( callback, args ){
 	Homey.log('on flow action.action.sound_alarm');
 	Homey.log('args', args);
 
-	Homey.manager('drivers').getDriver('alarmsound').capabilities.onoff.set(args.device, function (err, data) {
+	Homey.manager('drivers').getDriver('alarmsound').capabilities.onoff.set(args.device, true, function (err, data) {
 		Homey.log('');
 		Homey.log('Homey.manager(drivers).getDriver(alarmsound).capabilities.onoff.set');
 		Homey.log('err', err);
 		Homey.log('data', data);
+		if (err) callback (err, false);
 	});
 
 	callback( null, true ); // we've fired successfully
@@ -118,12 +128,28 @@ Homey.manager('flow').on('action.silence_alarm', function( callback, args ){
 	Homey.log('on flow action.action.silence_alarm');
 	Homey.log('args', args);
 
-	Homey.manager('drivers').getDriver('alarmsound').capabilities.onoff.set(args.device_data, function (err, data) {
+	Homey.manager('drivers').getDriver('alarmsound').capabilities.onoff.set(args.device, false, function (err, data) {
 		Homey.log('');
-		Homey.log('Homey.manager(drivers).getDriver(benext_alarmsound).capabilities.onoff.unset');
+		Homey.log('Homey.manager(drivers).getDriver(alarmsound).capabilities.onoff.set');
 		Homey.log('err', err);
 		Homey.log('data', data);
+		if (err) callback (err, false);
 	});
 
 	callback( null, true ); // we've fired successfully
 });
+
+module.exports.on('initNode', function( token ){
+	Homey.log('');
+	Homey.log('initNode');
+
+	var node = module.exports.nodes[ token ];
+	if( node ) {
+		Homey.log('node:', node);
+		/*
+		node.instance.CommandClass['COMMAND_CLASS_BASIC'].on('report', function( command, report ){
+			Homey.manager('flow').triggerDevice('FGD-212_s2', null, null, node.device_data);
+		});
+		*/
+	}
+})
