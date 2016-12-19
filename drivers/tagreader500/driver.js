adf619c6b61eecaca311e56b8caa37cbda844938
robertraaijmakers@gmail.com <robertraaijmakers@gmail.com>
2016-12-19 21:10:13 +0100

    Added device parameters

--- drivers/tagreader500/driver.js
+++ drivers/tagreader500/driver.js
@@ -6,7 +6,6 @@ const ZwaveDriver = require('homey-zwavedriver');
 // http://products.z-wavealliance.org/products/1979
 // http://www.benext.eu/static/manual/tagreader500.pdf
 
-//
 // Plan: When user gives an unknown tag. Search for the tag code in the settings of the benext app
 // When user is found, send the user ID back to the device
 // If user is not found, add the user to the settings with a new ID and reply this to the device
@@ -14,6 +13,9 @@ const ZwaveDriver = require('homey-zwavedriver');
 // But when this becomes available it's easily integrateble
 // The same way is for custom codes added by the user
 // On a setting page we can map the ID's we send out here to readable user names and use them in cards.
+
+var tagContainer = [ ]; // contains objects: { "tagId": 0, "tagValue": "" };
+var userContainer = [ ]; // contains objects: { "user": "bla", "tagIds": { 1, 3 } };
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
 debug: true,
@@ -49,25 +51,70 @@ debug: true,
 		},*/
 		'custom.gateway_control': {
 			'command_class'				: 'COMMAND_CLASS_ENTRY_CONTROL',
-			'command_get'				: function( report )
+			'command_get_parser'		: function( report )
 			{
 				console.log(report);
 				
-				/*
-				The supported events are: 
-				- Caching 
-				- Cached Keys 
-				- Enter 
-				- Arm Home 
-				- Arm Away 
-				- RFID 
-				*/
-				return;
+				var eventName = "none";
+				switch(eventName)
+				{
+					case "ARM_HOME": // (0x06)
+						// Announces home rfid/enter coming.
+						// As gateway we can allow or deny this request.
+							// Via a response: INDICATOR_SET ; ID: (0x06), value: (0x06) : allow
+							// or via: INDICATOR_SET; ID: (0x04), value: 8 : deny
+					break;
+					case "ARM_AWAY": // (0x05)
+						// Announces away rfid/enter coming.
+						// As gateway we can allow or deny this request.
+							// Via a response: INDICATOR_SET ; ID: (0x06), value: (0x06) : allow
+							// or via: INDICATOR_SET; ID: (0x04), value: 8 : deny
+					break;
+					case "RFID": // (0x0D)
+						// Contains a RFID tag in event data.
+					break;
+					case "ENTER":  // (0x0D)
+						// Contains array of numbers in event data.
+					break;
+					case "CACHING":  // (0x00)
+						// Announces a manual code is coming our way.
+					break;
+					case "CACHED_KEYS": // (0x01)
+						// event data contains array of numbers (toggle scene)
+					break;
+				}
 			}
 		}
     },
     settings: {
-		
+		"set_to_default" : {
+			"index": 0,
+			"size": 1
+		},
+		"feedback_time" : {
+			"index": 1,
+			"size": 1
+		},
+		"feedback_timeout" : {
+			"index": 2,
+			"size": 1
+		},
+		"feedback_beeps_per_second" : {
+			"index": 3,
+			"size": 1
+		},
+		"always_awake_mode" : {
+			"index": 4,
+			"size": 1
+		},
+		"operation_mode" : {
+			"index": 6,
+			"size": 1
+		},
+		"gateway_confirmation" : {
+			"index": 7,
+			"size": 1
+		}
     }
 });
 
