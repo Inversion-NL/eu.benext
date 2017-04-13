8260a6d4f95101947b0c75494f2aa528c7908b20
RobinBol <robinbolscher@gmail.com>
2017-04-13 16:17:26 +0200

    Plug-in Dimmer: add basic support (no settings/images/icons)

---
+++ drivers/plug_in_dimmer/driver.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const path = require('path');
+const ZwaveDriver = require('homey-zwavedriver');
+
+module.exports = new ZwaveDriver(path.basename(__dirname), {
+	capabilities: {
+		onoff: {
+			command_class: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
+			command_get: 'SWITCH_MULTILEVEL_GET',
+			command_set: 'SWITCH_MULTILEVEL_SET',
+			command_set_parser: value => ({
+				Value: (value > 0) ? 'on/enable' : 'off/disable',
+				'Dimming Duration': 'Factory default',
+			}),
+			command_report: 'SWITCH_MULTILEVEL_REPORT',
+			command_report_parser: report => {
+				if (report.Value === 'on/enable') return true;
+				else if (report.Value === 'off/disable') return false;
+				else if (typeof report.Value === 'number') return report.Value > 0;
+				else if (typeof report['Value (Raw)'] !== 'undefined') return report['Value (Raw)'][0] > 0;
+				return null;
+			},
+		},
+		dim: {
+			command_class: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
+			command_get: 'SWITCH_MULTILEVEL_GET',
+			command_set: 'SWITCH_MULTILEVEL_SET',
+			command_set_parser: (value, node) => {
+				module.exports.realtime(node.device_data, 'onoff', value > 0);
+
+				return {
+					Value: value * 99,
+					'Dimming Duration': 'Factory default',
+				};
+			},
+			command_report: 'SWITCH_MULTILEVEL_REPORT',
+			command_report_parser: (report, node) => {
+				if (report.Value === 'on/enable') {
+					module.exports.realtime(node.device_data, 'onoff', true);
+					return 1.0;
+				}
+				else if (report.Value === 'off/disable') {
+					module.exports.realtime(node.device_data, 'onoff', false);
+					return 0.0;
+				}
+				else if (typeof report.Value === 'number') {
+					module.exports.realtime(node.device_data, 'onoff', report.Value > 0);
+					return report.Value / 99;
+				}
+				else if (typeof report['Value (Raw)'] !== 'undefined') {
+					module.exports.realtime(node.device_data, 'onoff', report['Value (Raw)'][0] > 0);
+					if (report['Value (Raw)'][0] === 255) return 1.0;
+					return report['Value (Raw)'][0] / 99;
+				}
+				return null;
+			},
+		},
+		measure_power: {
+			command_class: 'COMMAND_CLASS_METER',
+			command_get: 'METER_GET',
+			command_get_parser: () => ({
+				'Sensor Type': 'Electric meter',
+				Properties1: {
+					Scale: 2,
+				},
+			}),
+			command_report: 'METER_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Properties2') &&
+					report.Properties2.hasOwnProperty('Scale') &&
+					report.Properties2['Scale'] === 2) {
+					console.log('W', report['Meter Value (Parsed)'])
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+			},
+		},
+		meter_power: {
+			command_class: 'COMMAND_CLASS_METER',
+			command_get: 'METER_GET',
+			command_get_parser: () => ({
+				'Sensor Type': 'Electric meter',
+				Properties1: {
+					Scale: 0,
+				},
+			}),
+			command_report: 'METER_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Properties2') &&
+					report.Properties2.hasOwnProperty('Scale') &&
+					report.Properties2['Scale'] === 0) {
+					console.log('kWh', report['Meter Value (Parsed)'])
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+			},
+		},
+	},
+	settings: {}
+});
