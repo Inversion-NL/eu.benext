d3048637c978287850f2dd22c952558cf3554165
RobinBol <robinbolscher@gmail.com>
2017-04-14 12:51:57 +0200

    P1 Dongle: refactor driver, remove not working settings (replaced them by poll interval settings), remove unused images and icons

--- drivers/p1_dongle/driver.js
+++ drivers/p1_dongle/driver.js
@@ -3,23 +3,20 @@
 const path = require('path');
 const ZwaveDriver = require('homey-zwavedriver');
 
-// http://www.cd-jackson.com/index.php/zwave/zwave-device-database/zwave-device-list/devicesummary/340
+// Documentation: http://www.benext.eu/static/manual/P1_dongle-nl.pdf
 
 module.exports = new ZwaveDriver(path.basename(__dirname), {
-	debug: true,
 	capabilities: {
 		measure_power: {
 			command_class: 'COMMAND_CLASS_METER',
 			command_get: 'METER_GET',
-			command_get_parser() {
-				return {
-					Properties1: {
-						'Rate Type': 'Import',
-						Scale: 2,
-					},
-					'Scale 2': 0,
-				};
-			},
+			command_get_parser: () => ({
+				Properties1: {
+					'Rate Type': 'Import',
+					Scale: 2,
+				},
+				'Scale 2': 0,
+			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
 				if (report.hasOwnProperty('Properties2')
@@ -32,17 +29,16 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 		'meter_power.normal': {
 			multiChannelNodeId: 1,
+			pollInterval: 'poll_interval_channel_1',
 			command_class: 'COMMAND_CLASS_METER',
 			command_get: 'METER_GET',
-			command_get_parser() {
-				return {
-					Properties1: {
-						'Rate Type': 'Import',
-						Scale: 0,
-					},
-					'Scale 2': 0,
-				};
-			},
+			command_get_parser: () => ({
+				Properties1: {
+					'Rate Type': 'Import',
+					Scale: 0,
+				},
+				'Scale 2': 0,
+			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
 				if (report.hasOwnProperty('Properties2')
@@ -55,17 +51,16 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 		'meter_power.low': {
 			multiChannelNodeId: 2,
+			pollInterval: 'poll_interval_channel_2',
 			command_class: 'COMMAND_CLASS_METER',
 			command_get: 'METER_GET',
-			command_get_parser() {
-				return {
-					Properties1: {
-						'Rate Type': 'Import',
-						Scale: 0,
-					},
-					'Scale 2': 0,
-				};
-			},
+			command_get_parser: () => ({
+				Properties1: {
+					'Rate Type': 'Import',
+					Scale: 0,
+				},
+				'Scale 2': 0,
+			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
 				if (report.hasOwnProperty('Properties2')
@@ -78,17 +73,16 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 		meter_gas: {
 			multiChannelNodeId: 3,
+			pollInterval: 'poll_interval_channel_3',
 			command_class: 'COMMAND_CLASS_METER',
 			command_get: 'METER_GET',
-			command_get_parser() {
-				return {
-					Properties1: {
-						'Rate Type': 'Import',
-						Scale: 0,
-					},
-					'Scale 2': 0,
-				};
-			},
+			command_get_parser: () => ({
+				Properties1: {
+					'Rate Type': 'Import',
+					Scale: 0,
+				},
+				'Scale 2': 0,
+			}),
 			command_report: 'METER_REPORT',
 			command_report_parser: report => {
 				if (report.hasOwnProperty('Properties2')
@@ -101,61 +95,13 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 		},
 	},
 	settings: {
-		HW_pullup: {
+		hw_pullup: {
 			index: 3,
 			size: 1,
 		},
 		baud_rate: {
 			index: 4,
 			size: 2,
-		},
-		channel_1_unasked_Report_use_w: {
-			index: 72,
-			size: 4,
-		},
-		channel_1_unasked_Report_use_kwh: {
-			index: 73,
-			size: 4,
-		},
-		channel_1_unasked_Report_returned_w: {
-			index: 74,
-			size: 4,
-		},
-		channel_1_unasked_Report_returned_kwh: {
-			index: 75,
-			size: 4,
-		},
-		channel_2_unasked_Report_use_w: {
-			index: 76,
-			size: 4,
-		},
-		channel_2_unasked_Report_use_kwh: {
-			index: 77,
-			size: 4,
-		},
-		channel_2_unasked_Report_returned_w: {
-			index: 78,
-			size: 4,
-		},
-		channel_2_unasked_Report_returned_kwh: {
-			index: 79,
-			size: 4,
-		},
-		channel_3_unasked_Report_use_w: {
-			index: 80,
-			size: 4,
-		},
-		channel_3_unasked_Report_use_kwh: {
-			index: 81,
-			size: 4,
-		},
-		channel_3_unasked_Report_returned_w: {
-			index: 82,
-			size: 4,
-		},
-		channel_3_unasked_Report_returned_kwh: {
-			index: 83,
-			size: 4,
 		},
 	},
 });
