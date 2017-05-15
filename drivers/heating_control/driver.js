6680ff3287c60bdb7bebd9cdbecbb05c145ad2aa
RobinBol <robinbolscher@gmail.com>
2017-05-15 12:23:00 +0200

    Heating Control: finish implementation (tested)

--- drivers/heating_control/driver.js
+++ drivers/heating_control/driver.js
@@ -5,47 +5,66 @@ const ZwaveDriver = require('homey-zwavedriver');
 
 // Documentation: http://www.benext.eu/static/manual/heatingcontrol.pdf
 
+const DEFAULT_SETPOINT = 20;
+
 module.exports = new ZwaveDriver(path.basename(__dirname), {
-	debug: true,
 	capabilities: {
 		target_temperature: {
-			command_class: 'COMMAND_CLASS_THERMOSTAT_SETPOINT',
-			command_get: 'THERMOSTAT_SETPOINT_GET',
-			command_get_parser: () => ({
-				'Level': {
-					'Setpoint Type': 'Heating 1',
-				}
-			}),
-			command_set: 'THERMOSTAT_SETPOINT_SET',
-			command_set_parser: (value) => {
+			command_class: 'COMMAND_CLASS_CLIMATE_CONTROL_SCHEDULE',
+			command_get: 'SCHEDULE_OVERRIDE_GET',
+			command_report: 'SCHEDULE_OVERRIDE_REPORT',
+			command_report_parser: report => {
+				console.log('SCHEDULE_OVERRIDE_REPORT -> report', report, 'override value', report['Override State (Raw)'][0]);
+				if (report &&
+					report.hasOwnProperty('Override State (Raw)')) {
 
-				// Create value buffer
-				const a = new Buffer(2);
-				a.writeUInt16BE((Math.round(value * 2) / 2 * 10).toFixed(0));
+					let delta;
+					// Value is below DEFAULT_SETPOINT
+					if (report['Override State (Raw)'][0] >= 126 && report['Override State (Raw)'][0] <= 256) {
+						delta = ((256 - report['Override State (Raw)'][0]) / 10) * -1;
+						console.log('calculated incoming (negative) delta', delta);
+					}
+					// Value is above DEFAULT_SETPOINT
+					else if (report['Override State (Raw)'][0] >= 0 && report['Override State (Raw)'][0] < 126) {
+						delta = report['Override State (Raw)'][0] / 10;
+						console.log('calculated incoming (positive) delta', delta);
+					}
+					console.log('SCHEDULE_OVERRIDE_REPORT -> calculated value ->', DEFAULT_SETPOINT + delta);
+					return Math.max(7, Math.min(DEFAULT_SETPOINT + delta, 32));
+				}
+			},
+			command_set: 'SCHEDULE_OVERRIDE_SET',
+			command_set_parser: (value, node) => {
+
+				console.log('Set target temperature', value);
+
+				let delta = (value - DEFAULT_SETPOINT) * 10;
+
+				console.log('SCHEDULE_OVERRIDE_SET -> calculated delta ->', delta);
+
+				if (delta < 0 && delta < -130) {
+					console.error('out_of_range_too_low', delta);
+					delta = -130;
+				} else if (delta > 0 && delta > 120) {
+					console.error('out_of_range_too_high', delta);
+					delta = 120;
+				}
+
+				if (delta < 0) delta = 256 + delta;
+
+				console.log('SCHEDULE_OVERRIDE_SET -> final object ->', {
+					Properties1: {
+						'Override Type': Number(node.settings.override_mode) || 1,
+					},
+					'Override State': new Buffer([delta])
+				});
 
 				return {
-					'Level': {
-						'Setpoint Type': 'Heating 1'
+					Properties1: {
+						'Override Type': Number(node.settings.override_mode) || 1,
 					},
-					'Level2': {
-						'Size': 2,
-						'Scale': 0,
-						'Precision': 1
-					},
-					'Value': a
-				};
-			},
-			command_report: 'THERMOSTAT_SETPOINT_REPORT',
-			command_report_parser: report => {
-				if (report.hasOwnProperty('Level2')
-					&& report.Level2.hasOwnProperty('Scale')
-					&& report.Level2.hasOwnProperty('Precision')
-					&& report.Level2['Scale'] === 0
-					&& report.Level2['Size'] !== 'undefined'
-					&& typeof report['Value'].readUIntBE(0, report.Level2['Size']) !== 'undefined') {
-					return report['Value'].readUIntBE(0, report.Level2['Size']) / Math.pow(10, report.Level2['Precision']);
+					'Override State': new Buffer([delta])
 				}
-				return null;
 			},
 		},
 		measure_temperature: {
@@ -97,5 +116,66 @@ module.exports = new ZwaveDriver(path.basename(__dirname), {
 			optional: true,
 		},
 	},
-	settings: {},
+	settings: {
+		override_mode: (newValue, oldValue, deviceData) => module.exports.nodes[deviceData.token].settings.override_mode = newValue
+	},
+});
+
+function configureDefaultSetpoint(deviceData, callback) {
+	console.log('configureDefaultSetpoint()');
+
+	Homey.wireless('zwave').getNode(deviceData, (err, node) => {
+		if (err) {
+			console.error('Error getting node', err);
+			return callback(err);
+		}
+
+		if (node && node && node.CommandClass && node.CommandClass.COMMAND_CLASS_THERMOSTAT_SETPOINT) {
+			node.CommandClass.COMMAND_CLASS_THERMOSTAT_SETPOINT.THERMOSTAT_SETPOINT_GET({
+				'Level': {
+					'Setpoint Type': 'Heating 1'
+				}
+			}, (err, result) => {
+				console.log('GET', err, result);
+				if (result && result.hasOwnProperty('Value')) {
+					let parsedValue = result['Value'].readUIntBE(0, 2);
+					if (typeof parsedValue === 'number' && parsedValue / 10 !== 20) {
+						node.CommandClass.COMMAND_CLASS_THERMOSTAT_SETPOINT.THERMOSTAT_SETPOINT_SET({
+							'Level': {
+								'Setpoint Type': 'Heating 1'
+							},
+							'Level2': {
+								'Size': 2,
+								'Scale': 0,
+								'Precision': 1
+							},
+							'Value': new Buffer([0, DEFAULT_SETPOINT * 10])
+						}, (err, result) => {
+							if (err) {
+								console.error('Error on THERMOSTAT_SETPOINT_SET', err);
+								return callback(err);
+							} else {
+								console.log('Success after THERMOSTAT_SETPOINT_SET', result);
+								return callback(null, true);
+							}
+						});
+					} else {
+						return callback(null, true);
+					}
+				}
+			});
+
+		} else {
+			return callback('missing_command_class_or_method');
+		}
+	});
+}
+
+module.exports.on('initNode', token => {
+	const node = module.exports.nodes[token];
+	if (node) {
+		configureDefaultSetpoint(node.device_data, (err, result) => {
+			console.log('initNode() -> configureDefaultSetpoint callback ->', err, result);
+		});
+	}
 });
