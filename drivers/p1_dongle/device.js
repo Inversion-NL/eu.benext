4f84369f7bf3880bf65d0d289348453799451d04
mapulu <46723804+mapulu@users.noreply.github.com>
2019-07-26 06:16:24 +0200

    Update device.js

--- drivers/p1_dongle/device.js
+++ drivers/p1_dongle/device.js
@@ -13,12 +13,48 @@ onMeshInit() {
 
 this.registerCapability('measure_power', 'METER');
 this.registerCapability('meter_power.normal', 'METER', {
-            multiChannelNodeId: 1,
+			multiChannelNodeId: 1,
 			pollInterval: 'poll_interval_channel_1',
+			command_class: 'COMMAND_CLASS_METER',
+			command_get: 'METER_GET',
+			command_get_parser: () => ({
+				Properties1: {
+					'Rate Type': 'Import',
+					Scale: 0,
+				},
+				'Scale 2': 0,
+			}),
+			command_report: 'METER_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Properties2')
+					&& report.Properties2.hasOwnProperty('Scale bits 10')
+					&& report.Properties2['Scale bits 10'] === 0) {
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+}
 });
 this.registerCapability('meter_power.low', 'METER', {
-            multiChannelNodeId: 2,
+			multiChannelNodeId: 2,
 			pollInterval: 'poll_interval_channel_2',
+			command_class: 'COMMAND_CLASS_METER',
+			command_get: 'METER_GET',
+			command_get_parser: () => ({
+				Properties1: {
+					'Rate Type': 'Import',
+					Scale: 0,
+				},
+				'Scale 2': 0,
+			}),
+			command_report: 'METER_REPORT',
+			command_report_parser: report => {
+				if (report.hasOwnProperty('Properties2')
+					&& report.Properties2.hasOwnProperty('Scale bits 10')
+					&& report.Properties2['Scale bits 10'] === 0) {
+					return report['Meter Value (Parsed)'];
+				}
+				return null;
+}
 });
 this.registerCapability('meter_gas', 'METER', {
 			multiChannelNodeId: 3,
