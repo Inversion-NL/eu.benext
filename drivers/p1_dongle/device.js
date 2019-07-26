c45e9b402009b3abe9fe0c9f59eca479efacf96b
mapulu <46723804+mapulu@users.noreply.github.com>
2019-07-26 08:49:26 +0200

    Update device.js

--- drivers/p1_dongle/device.js
+++ drivers/p1_dongle/device.js
@@ -15,17 +15,16 @@ this.registerCapability('measure_power', 'METER');
 this.registerCapability('meter_power.normal', 'METER', {
 			multiChannelNodeId: 1,
 			pollInterval: 'poll_interval_channel_1',
-			command_class: 'COMMAND_CLASS_METER',
-			command_get: 'METER_GET',
-			command_get_parser: () => ({
+			get: 'METER_GET',
+			getParser: () => ({
 				Properties1: {
 					'Rate Type': 'Import',
 					Scale: 0,
 				},
 				'Scale 2': 0,
 			}),
-			command_report: 'METER_REPORT',
-			command_report_parser: report => {
+			report: 'METER_REPORT',
+			reportParser: report => {
 				if (report.hasOwnProperty('Properties2')
 					&& report.Properties2.hasOwnProperty('Scale bits 10')
 					&& report.Properties2['Scale bits 10'] === 0) {
@@ -37,17 +36,16 @@ this.registerCapability('meter_power.normal', 'METER', {
 this.registerCapability('meter_power.low', 'METER', {
 			multiChannelNodeId: 2,
 			pollInterval: 'poll_interval_channel_2',
-			command_class: 'COMMAND_CLASS_METER',
-			command_get: 'METER_GET',
-			command_get_parser: () => ({
+			get: 'METER_GET',
+			getParser: () => ({
 				Properties1: {
 					'Rate Type': 'Import',
 					Scale: 0,
 				},
 				'Scale 2': 0,
 			}),
-			command_report: 'METER_REPORT',
-			command_report_parser: report => {
+			report: 'METER_REPORT',
+			reportParser: report => {
 				if (report.hasOwnProperty('Properties2')
 					&& report.Properties2.hasOwnProperty('Scale bits 10')
 					&& report.Properties2['Scale bits 10'] === 0) {
