90b069dcf3dd49351036f0c0f2a9da3918cf4984
mapulu <46723804+mapulu@users.noreply.github.com>
2019-08-26 19:48:05 +0200

    Fix Molite Sensor

--- drivers/molite_sensor/device.js
+++ drivers/molite_sensor/device.js
@@ -3,7 +3,8 @@
 const Homey = require('homey');
 const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
-// Documentation: http://www.benext.eu/static/manual/molitesensor-nl.pdf
+// Documentation NL: http://www.benext.eu/static/manual/molitesensor-nl.pdf
+// English: http://www.benext.eu/static/manual/molitesensor.pdf
 
 class MoliteSensorDevice extends ZwaveDevice {
 
@@ -11,7 +12,23 @@ onMeshInit() {
 		this.enableDebug();
 		this.printNode();
 this.registerCapability('alarm_motion', 'SENSOR_BINARY');
-this.registerCapability('alarm_tamper', 'SENSOR_ALARM');	
+this.registerCapability('alarm_motion', 'BASIC');
+this.registerCapability('alarm_tamper', 'ALARM', {
+			get: 'ALARM_GET',
+			getParser: () => ({
+				'Alarm Type': 3,
+			}),
+			report: 'ALARM_REPORT',
+			reportParser: report => {
+				if (report &&
+				    report.hasOwnProperty('Alarm Type') &&
+				    report['Alarm Type'] === 3 &&
+				    report.hasOwnProperty('Alarm Level')) {
+					return report['Alarm Level'] === 255;
+				}
+				return null;
+			}
+});
 this.registerCapability('measure_temperature', 'SENSOR_MULTILEVEL');
 this.registerCapability('measure_luminance', 'SENSOR_MULTILEVEL');
 this.registerCapability('measure_battery', 'BATTERY');
